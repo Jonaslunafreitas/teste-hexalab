2a550b35b70175200c89ab1fbb19dbd0a2f701e3
Jonas Luna <jonasluna.cont@gmail.com>
2021-08-29 19:45:30 -0300

    feat:Logica de calculo com Jquery

---
+++ script.js
@@ -0,0 +1,78 @@
+$('document').ready(function () {
+
+
+    // $("#formGorjeta").submit(function(e){
+
+    // });
+
+    $(".btn-porcentagem").click(function (e) {
+        const value = this.value;
+        removerClasseBtnPorcentagem();
+        $(this).addClass("btn-porcentagem-selecionado")
+        $("#personalizado").val(value)
+    });
+
+    $("#personalizado").keypress(function (e) {
+        removerClasseBtnPorcentagem();
+    })
+
+    $(`#btn-calcular`).click(function (e) {
+        const ehPersonlizadoValidoLocal = ehPersonlizadoValido();
+        const ehTotalPessoasValidoLocal = ehTotalPessoasValido();
+        const ehValorPedidoValidoLocal = ehValorPedidoValido();
+        if (ehPersonlizadoValidoLocal && ehTotalPessoasValidoLocal && ehValorPedidoValidoLocal) {
+            const valorPedido = parseFloat($(`#valor-pedido`).val());
+            const personalizado = parseFloat($(`#personalizado`).val());
+            const totalPessoas = parseFloat($(`#total-pessoas`).val());
+            const valorTotal = valorPedido * (personalizado/100);
+            $("#valor-total-gorjeta").val(valorTotal.toFixed(2));
+            const valorPessoa =  valorTotal / totalPessoas;
+            $("#valor-pessoa-gorjeta").val(valorPessoa.toFixed(2));
+        }
+    });
+
+});
+
+
+function removerClasseBtnPorcentagem() {
+    $('.btn-porcentagem-selecionado').each(function (i, obj) {
+        $(this).removeClass("btn-porcentagem-selecionado")
+    });
+}
+
+function ehValorPedidoValido() {
+    const valorPedido = $(`#valor-pedido`).val();
+    if (!!valorPedido) {
+        $(`#valor-pedido`).removeClass("is-invalid");
+        return true
+    }
+    $(`#valor-pedido`).addClass("is-invalid")
+    return false;
+}
+
+
+function ehPersonlizadoValido() {
+    const personalizado = $(`#personalizado`).val();
+    if (!!personalizado) {
+        $(`#personalizado`).removeClass("is-invalid");
+        return true
+    }
+    $(`#personalizado`).addClass("is-invalid")
+    return false;
+}
+
+
+function ehTotalPessoasValido() {
+    const totalPessoas = $(`#total-pessoas`).val();
+    if (!!totalPessoas) {
+        $(`#total-pessoas`).removeClass("is-invalid");
+        $(`#total-pessoas-icone`).attr("src", "img/icone-verde.svg");
+        return true
+    }
+    $(`#total-pessoas`).addClass("is-invalid");
+    $(`#total-pessoas-icone`).attr("src", "img/icone-vermelho.svg");
+    return false;
+
+}
+
+
